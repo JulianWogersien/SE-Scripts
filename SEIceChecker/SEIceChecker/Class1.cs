f979f6eed3c2970f6942658329a0e79a5d38e39d
Coolian <julian.wogersien@gmail.com>
2024-12-26 15:03:03 +0100

    Ice Checker script

---
+++ SEIceChecker/SEIceChecker/Class1.cs
@@ -0,0 +1,92 @@
+﻿using Sandbox.Game.GameSystems;
+using Sandbox.ModAPI.Ingame;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VRage.Game.ModAPI.Ingame;
+using VRageMath;
+
+namespace SpaceEngineersScript
+{
+    public class Program : MyGridProgram
+    {
+        // CONFIG
+        private const double ICE_WARNING_THRESHOLD = 10000;
+
+        public Program()
+        {
+            Runtime.UpdateFrequency = UpdateFrequency.Update100;
+        }
+
+        public void Main(string argument, UpdateType updateSource)
+        {
+            double totalIce = 0;
+            List<IMyTerminalBlock> blocks = new List<IMyTerminalBlock>();
+            GridTerminalSystem.GetBlocks(blocks);
+
+            var mainGridId = Me.CubeGrid.EntityId;
+
+            foreach (var block in blocks)
+            {
+                if (block.CubeGrid.EntityId != mainGridId) continue;
+                if (!(block is IMyTerminalBlock)) continue;
+                IMyInventory inventory = ((IMyTerminalBlock)block).GetInventory(0);
+                if (inventory == null) continue;
+                var items = new List<MyInventoryItem>();
+                inventory.GetItems(items);
+                foreach (var item in items)
+                {
+                    if (item.Type.SubtypeId.Contains("Ice"))
+                    {
+                        totalIce += (double)item.Amount;
+                    }
+                }
+            }
+
+            List<IMyLightingBlock> lights = new List<IMyLightingBlock>();
+            GridTerminalSystem.GetBlocksOfType(lights);
+            lights.RemoveAll(light => light.CubeGrid.EntityId != mainGridId);
+
+            if (totalIce <= ICE_WARNING_THRESHOLD)
+            {
+                foreach (var light in lights)
+                {
+                    if (string.IsNullOrEmpty(light.CustomData))
+                    {
+                        light.CustomData = $"OriginalColor:{light.Color.R},{light.Color.G},{light.Color.B}";
+                    }
+
+                    light.Color = Color.Red;
+                    light.BlinkLength = 50f;
+                    light.BlinkIntervalSeconds = 0.5f;
+                    light.Enabled = true;
+                }
+                Echo($"WARNING: Low ice level detected!\nCurrent ice: {totalIce:N0} kg\nThreshold: {ICE_WARNING_THRESHOLD:N0} kg");
+            }
+            else
+            {
+                foreach (var light in lights)
+                {
+                    if (!string.IsNullOrEmpty(light.CustomData))
+                    {
+                        string[] colorData = light.CustomData.Split(':')[1].Split(',');
+                        if (colorData.Length == 3)
+                        {
+                            light.Color = new Color(
+                                byte.Parse(colorData[0]),
+                                byte.Parse(colorData[1]),
+                                byte.Parse(colorData[2])
+                            );
+                        }
+                        light.CustomData = "";
+                    }
+                    light.BlinkLength = 0f;
+                    light.BlinkIntervalSeconds = 0f;
+                }
+                Echo($"Ice levels normal\nCurrent ice: {totalIce:N0} kg");
+            }
+        }
+    }
+}
---
+++ SEIceChecker/packages/Mal.Mdk2.References.2.2.3/readme.md
@@ -0,0 +1,51 @@
+﻿# MDK References
+
+---
+
+### Note: It does seem like VS2022 is required for stable performance. However this is unconfirmed.
+
+The game [Space Engineers](https://store.steampowered.com/app/244850/Space_Engineers/)
+(by [Keen Software House](https://www.keenswh.com/), no affiliation) supports 
+scripting and modding using the C# language. This package aims to automatically detect
+the location of the Space Engineers installation, and referencing the assembly DLLs required
+to write scripts and mods.
+
+_This package is part of the MDK2 project._
+
+---
+
+### Usage:
+Simply reference the nuget package in your mod project. It should automatically do its thing.
+
+### Copying DLLs locally
+By default, the assemblies are referenced directly from their location in the Space Engineers
+game installation folder. If you want to run unit tests, you will need to have those assemblies
+copied locally. To do so, add the following to the initial PropertyGroup in your project file:
+```xml
+ <SpaceEngineersBinCopyLocal>true</SpaceEngineersBinCopyLocal>
+```
+**Keep in mind:** These assemblies belong to Keen Software House. You are _not_ allowed to 
+pack and redistribute these dlls, this is the entire reason why this package exist to automate
+their reference for you.
+
+### Overriding the binary path
+To manually specify where to get the Space Engineers binaries from, add a file named 
+`<projectname>.mdk.local.ini` to the project directory. For example; if your project is named 
+`MyMod`, the file should be named `MyMod.mdk.local.ini`. In this file, add the following line:
+```
+[mdk]
+binarypath = C:\Path\To\SpaceEngineers\Bin
+```
+
+Note: Make sure you add this file to your `.gitignore` file, or any other version control ignore 
+file you use. The binary path is usually different for each developer. If, against all odds, this 
+does not apply to you, you should use the file name pattern `<projectname>.mdk.ini` instead.
+
+---
+
+_Disclaimer:_
+
+_These tools are an independent creation and is not endorsed, sponsored, nor affiliated with Keen Software House.
+"Space Engineers" is a trademark of Keen Software House. All trademarks and copyrights used are properties of their
+respective owners. The use of "Space Engineers" in these tools is for reference purposes only and does not imply
+any association or endorsement._
